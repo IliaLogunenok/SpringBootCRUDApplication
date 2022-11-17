e60d4468768a93af6e9e711d242c1de43cacf915
IliaLogunenok <ilyajazzrock@gmail.com>
2022-11-17 19:19:20 +0700

    removed dao class

--- src/main/java/web/SpringBootCRUDApplication/service/UserServiceImpl.java
+++ src/main/java/web/SpringBootCRUDApplication/service/UserServiceImpl.java
@@ -2,40 +2,44 @@ package web.SpringBootCRUDApplication.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import web.SpringBootCRUDApplication.UserDAO.UserDAO;
 import web.SpringBootCRUDApplication.model.User;
+import web.SpringBootCRUDApplication.repositories.UserRepository;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
 
-    private final UserDAO userDAO;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserServiceImpl(UserDAO userDAO) {
-        this.userDAO = userDAO;
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     public List<User> findAll() {
-        return userDAO.findAll();
+        return userRepository.findAll();
     }
 
     public User findOne(int id) {
-        return userDAO.findOne(id);
+        Optional<User> foundUser = userRepository.findById(id);
+
+        return foundUser.orElse(null);
     }
 
     public void save(User user) {
-        userDAO.save(user);
+        userRepository.save(user);
     }
 
     public void update(int id, User updateUser) {
-        userDAO.update(id, updateUser);
+        updateUser.setId(id);
+        userRepository.save(updateUser);
     }
 
     public void delete(int id) {
-        userDAO.delete(id);
+        userRepository.deleteById(id);
     }
 }
